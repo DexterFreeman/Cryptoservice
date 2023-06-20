eb9602e87e91143008601b340db95e68844c4267
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-06-20 16:50:57 +0100

    Got the predictions working, its now integrated with the app and now shows in the latest-prediction page, also fixed some project structure issues

--- prediction_app/predictions/urls.py
+++ prediction_app/predictions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import latest_prediction
+from .views import latest_prediction, create_prediction
 
 urlpatterns = [
     path('latest-prediction/', latest_prediction, name='latest_prediction'),
+    path('create-prediction/', create_prediction, name='create_prediction')
 ]
