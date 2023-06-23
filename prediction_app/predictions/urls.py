f147476fd55599af850fe8b99bee2806bbf77e30
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-06-23 17:00:33 +0100

    Changed django rest api, can now send in requests for specific coins

--- prediction_app/predictions/urls.py
+++ prediction_app/predictions/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import latest_prediction, create_prediction, PredictionApiView
+from .views import latest_prediction, create_prediction, PredictionApiView, LatestPredictionApiView, get_prediction
 
 urlpatterns = [
     path('latest-prediction/', latest_prediction, name='latest_prediction'),
     path('create-prediction/', create_prediction, name='create_prediction'),
-    path('api/', PredictionApiView.as_view())
+    path('api/all', PredictionApiView.as_view()),
+    path('api/latest', LatestPredictionApiView.as_view()),
+    path('get/', get_prediction, name="get_prediction"),
 ]
