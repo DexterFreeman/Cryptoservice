b999850147e78b7ea40366cca049a9dec9a8e0fc
DexterFreeman <dexter.freeman10@hotmail.co.uk>
2023-06-28 10:42:47 +0100

    adding scheduled task

--- bot/src/main/java/BotListener.java
+++ bot/src/main/java/BotListener.java
@@ -1,3 +1,6 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +13,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.Channel;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class BotListener extends ListenerAdapter {
@@ -18,7 +28,76 @@ public class BotListener extends ListenerAdapter {
 
     @Override
     public void onReady(@NotNull ReadyEvent event) {
-        super.onReady(event);
+
+        StringBuffer responseContent = new StringBuffer();
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/London"));
+        ZonedDateTime scheduledTime = now.withHour(16).withMinute(45).withSecond(0);
+
+        //if its already past the scheduled time
+        if (now.compareTo(scheduledTime) > 0) {
+            scheduledTime = scheduledTime.plusDays(1);
+        }
+
+        Duration durationUntilScheduledTime = Duration.between(now, scheduledTime);
+        long initialDelayScheduledTime = durationUntilScheduledTime.getSeconds();
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            System.out.println("Scheduled task running");
+            JDA jda = event.getJDA();
+            Guild guild = jda.getGuildById("1123175549435641866");
+            TextChannel textChannel = guild.getTextChannelById("1123175549435641869");
+            for (String currency : BotUtils.crypto_currencies_list
+            ) {
+                String line = null;
+                BufferedReader reader = null;
+                JSONObject jsonObject = null;
+                responseContent.delete(0, responseContent.length());
+                System.out.println("http://127.0.0.1:8000/predictions/get/?currency=" + currency);
+                try {
+                    URL url = new URL("http://127.0.0.1:8000/predictions/get/?currency=" + currency);
+                    connection = (HttpURLConnection) url.openConnection();
+
+                    //Request setup
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(500000);
+
+                    int status = connection.getResponseCode();
+                    System.out.println(status);
+                    if (status > 299) {
+
+                        reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                        while ((line = reader.readLine()) != null) {
+                            responseContent.append(line);
+                        }
+                        reader.close();
+                    } else {
+
+                        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        while ((line = reader.readLine()) != null) {
+                            responseContent.append(line);
+                        }
+                        reader.close();
+                    }
+
+                    System.out.println(responseContent.toString());
+                    jsonObject = new JSONObject(responseContent.toString());
+                    System.out.println(jsonObject);
+                    textChannel.sendMessage("Currency: " + jsonObject.get("currency")).queue();
+                    textChannel.sendMessage("Predicted price: " + jsonObject.get("predicted_price")).queue();
+
+
+                } catch (MalformedURLException e) {
+                    System.out.println(e.getMessage());
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            }
+        }, initialDelayScheduledTime, TimeUnit.DAYS.toSeconds(1), TimeUnit.SECONDS);
+
     }
 
     @Override
